c78e1d132158f2ebe50ac5eb797a96b1512ceee8
Jille Timmermans <jille@quis.cx>
2023-02-22 10:21:51 +0100

    minmax: Add method Godoc

--- minmax.go
+++ minmax.go
@@ -2,6 +2,7 @@ package genericz
 
 import "golang.org/x/exp/constraints"
 
+// Min returns the lowest value given.
 func Min[T constraints.Ordered](a ...T) T {
 	ret := a[0]
 	for _, e := range a[1:] {
@@ -12,6 +13,7 @@ func Min[T constraints.Ordered](a ...T) T {
 	return ret
 }
 
+// Max returns the highest value given.
 func Max[T constraints.Ordered](a ...T) T {
 	ret := a[0]
 	for _, e := range a[1:] {
